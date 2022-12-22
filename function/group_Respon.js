74a7b21dce1b8c65276d8e75f57cd209e426caf3
Icontol <1sansowner@gmail.com>
2022-12-22 11:13:25 +0700

    permen-ricola

---
+++ function/group_Respon.js
@@ -0,0 +1,111 @@
+//GUA LEXXY SUKA SKR
+//SIMPLE KEREN RAPIH
+//CREATED BY LEXXY OFFICIAL
+
+const fs = require('fs')
+
+exports.groupResponse_Remove = async (conn, update) => {
+try {
+ppuser = await conn.profilePictureUrl(num, 'image')
+} catch {
+ppuser = 'https://telegra.ph/file/265c672094dfa87caea19.jpg'
+}
+const metadata = await conn.groupMetadata(update.id)
+for (let participant of update.participants) {
+try{
+let metadata = await conn.groupMetadata(update.id)
+let participants = update.participants
+for (let num of participants) {
+if (update.action == 'remove'){
+var button = [{ buttonId: '!text_grup', buttonText: { displayText: 'bye beban grup👋'}, type: 1 }]
+await conn.sendMessage(
+update.id, 
+{
+text: `*selamt tinggal @${num.split("@")[0]}*\n*kalo mau balik lagi ke group ini, wajib donasi pulsa 10K*\n\nmake doang kaga donasi?\nenak banget 🗿\ngw buat lama lu tinggal make 🗿`,
+buttons: button,
+footer: metadata.subject, 
+mentions: [num] })
+}
+}
+} catch (err) {
+console.log(err)
+}
+}   
+}
+  
+exports.groupResponse_Welcome = async (conn, update) => {
+try {
+ppuser = await conn.profilePictureUrl(num, 'image')
+} catch {
+ppuser = 'https://telegra.ph/file/265c672094dfa87caea19.jpg'
+}
+const metadata = await conn.groupMetadata(update.id)   
+for (let participant of update.participants) {
+try{
+let metadata = await conn.groupMetadata(update.id)
+let participants = update.participants
+for (let num of participants) {
+if (update.action == 'add') {
+var button = [{ buttonId: '!text_grup', buttonText: { displayText: 'Welcome👋'}, type: 1 }]
+await conn.sendMessage(
+update.id, 
+{ 
+text: `*hai @${num.split("@")[0]}*\n*selamat datang di ${metadata.subject}* \n\n*Jangan Lupa Intro*\n*Nama:*\n*Umur:*\n*Hobi:*\n*Askot:*\n\n*Sering² Baca Deskripsi Ya Kak🙏*\n\njangan lupa donasi tod 🗿\nmake bot doang kaga bantu donasi?\nenak bener 🗿`,
+buttons: button, 
+footer: metadata.subject,
+mentions: [num] })
+}
+}
+} catch (err) {
+console.log(err)
+}
+}   
+}
+  
+exports.groupResponse_Promote = async (conn, update) => {  
+const metadata = await conn.groupMetadata(update.id)   
+for (let participant of update.participants) {
+try{
+let metadata = await conn.groupMetadata(update.id)
+let participants = update.participants
+for (let num of participants) {
+if (update.action == 'promote') {
+var button = [{ buttonId: '!text_grup', buttonText: { displayText: 'Selamat🎉'}, type: 1 }]
+await conn.sendMessage(
+update.id, 
+{ 
+text: `*@${num.split("@")[0]} Naik jabatan jadi admin grup*`,
+buttons: button, 
+footer: metadata.subject,
+mentions: [num] })
+}
+}
+} catch (err) {
+console.log(err)
+}
+}   
+}
+  
+exports.groupResponse_Demote = async (conn, update) => {  
+const metadata = await conn.groupMetadata(update.id)   
+for (let participant of update.participants) {
+try{
+let metadata = await conn.groupMetadata(update.id)
+let participants = update.participants
+for (let num of participants) {
+if (update.action == 'demote') {
+var button = [{ buttonId: '!text_grup', buttonText: { displayText: 'mampus😂'}, type: 1 }]
+await conn.sendMessage(
+update.id, 
+{ 
+text: `*@${num.split("@")[0]} Turun jabatan jadi member grup*`,
+buttons: button, 
+footer: metadata.subject,
+mentions: [num] })
+}
+}
+} catch (err) {
+console.log(err)
+}
+}   
+}
